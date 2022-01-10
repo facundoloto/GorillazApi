3121e2ae391bdd2e8352738aecebf84addf9cbd8
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2022-01-10 04:19:05 -0300

    Update Character.jsx

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -12,7 +12,7 @@ getMemberApi()
 },[])
 const getMemberApi= async ()=>{
   console.log(ruta)
-    const DataMember=await fetch(`http://app-b869980c-2c89-4f3d-b005-99fa43dc174e.cleverapps.io/apigorillaz/v1/${ruta}/${id}`)
+    const DataMember=await fetch(`https://app-b869980c-2c89-4f3d-b005-99fa43dc174e.cleverapps.io/apigorillaz/v1/${ruta}/${id}`)
     const DataJson=await DataMember.json()
     console.log(DataJson)
     setData(DataJson)
